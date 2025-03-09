ab642707460d3fb1a84c0eb56c559eda5296de56
kimo5ab3 <kimosabe.mr@gmail.com>
2025-03-09 23:42:58 +0100

    give some order

--- src/main.c
+++ src/main.c
@@ -1,42 +1,6 @@
-
+#include "sqlite/sqlite3.h"
 #include "stdio.h"
-#include "sqlite3.h"
-#include <stdlib.h>
-#include "sqlqueries.c"
-
-
-
-int check_sqlite_version(){
-  int res = system("sqlite3 --version");
-  if (res != 0) {
-    printf(
-        "TODO cli app use sqlite3, plese install sqlite3 and run TODO again");
-    return 1;
-  }
-  return 0;
-};
-
-int open_db(sqlite3 *db){
-  int rc = sqlite3_open("todo_db", &db);
-  if(rc){
-    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    return 1;
-  }
-  return 0;
-};
-
-int check_table_presence(sqlite3 *db){
-  int rc;
-  char *zErrMsg = 0;
-  rc = sqlite3_exec(db, check_table, NULL, NULL, &zErrMsg);
-  if(rc != SQLITE_OK){
-    printf("Table todo NOT exist %s \n", zErrMsg);
-    sqlite3_free(zErrMsg);
-    return 1;
-  } else {
-    return 0;
-  }
-};
+#include "db_query.c"
 
 
 
@@ -44,27 +8,39 @@ int check_table_presence(sqlite3 *db){
 
 int main() {
   sqlite3 *db;
-  int todo_table;
-  int fres;
-  char *zErrMsg = 0;
+  char *err_msg = 0;
+  char selection; 
 
-
-  fres = check_sqlite_version();
-  if(fres) return fres;
-
-  fres = open_db(db);
-  if(fres) return fres;
-
-  todo_table = check_table_presence(db);
-
-  if(todo_table){
-    fres = sqlite3_exec(db, create_todo_table, NULL, 0, &zErrMsg);
-    if(fres != SQLITE_OK){
-      printf("Error on create todo table %s \n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    }
-  }
   
-  sqlite3_close(db);
+  int oc = sqlite3_open("todo.db", &db);
+
+  if(oc != SQLITE_OK){
+    printf("Errore nell'apertura del db %s \n", sqlite3_errmsg(db));
+    return 1;
+  }
+
+  const char create_todo_table[] = "CREATE TABLE IF NOT EXISTS todos( "
+                                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                      "description TEXT,"
+                                      "status TEXT,"
+                                      "date DATE,"
+                                      "end_date DATE);";
+
+  oc = sqlite3_exec(db, create_todo_table, 0, 0, &err_msg);
+
+  if(oc != SQLITE_OK){
+    printf("Error on create TODO table");
+    return 1;
+  }
+
+  printf("What do you want to do? \n");
+  printf("[show] - Show all TODOS (if exists) \n");
+  printf("[create] - Create new TODO \n");
+  printf("[edit] - Edit TODO (need id of TODO) \n");
+  printf("[delete] - Delete TODO (need id of TODO) \n");
+  printf("[complete] - Change on COMPLETE status of TODO (need id of TODO) \n");
+  printf("[reset] - Change on AVAILABLE status of TODO (need id of TODO) \n");
+  scanf("%s", &selection);
+
   return 0;
 }
