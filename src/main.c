a7b61ffa5e3ebd0600e255ee8c915baae1c4930c
kimo5ab3 <kimosabe.mr@gmail.com>
2025-03-10 00:16:38 +0100

    start define command

--- src/main.c
+++ src/main.c
@@ -1,37 +1,35 @@
 #include "sqlite/sqlite3.h"
 #include "stdio.h"
-#include "db_query.c"
-
-
-
-
+#include <string.h>
 
 int main() {
   sqlite3 *db;
   char *err_msg = 0;
-  char selection; 
+  char command[8];
+  int prg_exit = 0;
 
-  
   int oc = sqlite3_open("todo.db", &db);
 
-  if(oc != SQLITE_OK){
+  if (oc != SQLITE_OK) {
     printf("Errore nell'apertura del db %s \n", sqlite3_errmsg(db));
     return 1;
   }
 
   const char create_todo_table[] = "CREATE TABLE IF NOT EXISTS todos( "
-                                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                                      "description TEXT,"
-                                      "status TEXT,"
-                                      "date DATE,"
-                                      "end_date DATE);";
+                                   "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                   "description TEXT,"
+                                   "status TEXT,"
+                                   "date DATE,"
+                                   "end_date DATE);";
 
   oc = sqlite3_exec(db, create_todo_table, 0, 0, &err_msg);
 
-  if(oc != SQLITE_OK){
+  if (oc != SQLITE_OK) {
     printf("Error on create TODO table");
     return 1;
   }
+
+  printf("\033c");
 
   printf("What do you want to do? \n");
   printf("[show] - Show all TODOS (if exists) \n");
@@ -40,7 +38,46 @@ int main() {
   printf("[delete] - Delete TODO (need id of TODO) \n");
   printf("[complete] - Change on COMPLETE status of TODO (need id of TODO) \n");
   printf("[reset] - Change on AVAILABLE status of TODO (need id of TODO) \n");
-  scanf("%s", &selection);
+  scanf("%s", command);
 
+  int command_match;
+
+  if (strcmp(command, "show")) {
+    command_match = 1;
+    // show
+  }
+
+  if (strcmp(command, "create")) {
+    command_match = 1;
+    // create
+  }
+
+  if (strcmp(command, "edit")) {
+    command_match = 1;
+    // edit
+  }
+
+  if (strcmp(command, "delete")) {
+    command_match = 1;
+    // delete
+  }
+
+  if (strcmp(command, "complete")) {
+    command_match = 1;
+    // complete
+  }
+
+  if (strcmp(command, "reset")) {
+    command_match = 1;
+    // reset
+  }
+
+  if (command_match == 0) {
+    // error on insert option
+    printf("Comand not found");
+    prg_exit = 1;
+  }
+
+  sqlite3_close(db);
   return 0;
 }
